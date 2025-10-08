58e86ec61ec25c6dee9c0987f985f7d783f4d0d2
Arman Mir <mirarman8583@gmail.com>
2025-10-08 17:20:16 +0600

    enhance Error404 and ErrorApp components with improved layout and messaging

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import errorImg from '../assets/error-404.png'
 
 const Error404 = () => {
     return (
-        <div>
-            <h2>Error 404</h2>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 text-center">
+
+   
+            <img
+                src={errorImg}
+                alt="404 illustration"
+                className="w-full max-w-md mb-8"
+            />
+
+  
+            <h1 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-2">
+                Oops, page not found!
+            </h1>
+
+  
+            <p className="text-gray-600 mb-6">
+                The page you are looking for is not available.
+            </p>
+
+
+            <button
+                className="bg-gradient-to-r from-indigo-500 via-purple-600 to-purple-500 text-white px-6 py-2 rounded-md text-sm transition cursor-pointer"
+            >
+                Go Back!
+            </button>
         </div>
     );
 };
--- src/Pages/ErrorApp.jsx
+++ src/Pages/ErrorApp.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import errorImg from '../assets/App-Error.png'
 
 const ErrorApp = () => {
     return (
-        <div>
-           <h2>Error App</h2> 
-        </div>
+         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 text-center">
+        
+           
+                    <img
+                        src={errorImg}
+                        alt="404 illustration"
+                        className="w-full max-w-md mb-8"
+                    />
+        
+          
+                    <h1 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-2">
+                        OPPS!! APP NOT FOUND
+                    </h1>
+        
+          
+                    <p className="text-gray-600 mb-6">
+                        The App you are requesting is not found on our system.  please try another apps.
+                    </p>
+        
+        
+                    <button
+                        className="bg-gradient-to-r from-indigo-500 via-purple-600 to-purple-500 text-white px-6 py-2 rounded-md text-sm transition cursor-pointer"
+                    >
+                        Go Back!
+                    </button>
+                </div>
     );
 };
 
