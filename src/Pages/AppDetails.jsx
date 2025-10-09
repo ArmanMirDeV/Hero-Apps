eda8a9fdd3d725a6d80a1bc78ce82a491c644e4e
Arman Mir <mirarman8583@gmail.com>
2025-10-09 17:42:12 +0600

    Add react-toastify for installation success notifications in AppDetails component

--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router';
 import useAppDetails from '../Hooks/useAppDetails';
 import Loading from '../Components/Loading';
 import { Bar, BarChart, CartesianGrid, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { ToastContainer, toast } from 'react-toastify';
 
 const AppDetails = () => {
 
@@ -43,6 +44,7 @@ const AppDetails = () => {
         const updatedApp = [...existingApp, singleAppData];
         localStorage.setItem('app', JSON.stringify(updatedApp));
         setIsInstalled(true);
+        toast("Installed Successfully")
     };
 
     return (
@@ -87,38 +89,38 @@ const AppDetails = () => {
 
             </div>
 
-           <div className='space-y-3' >
-            <h3 className='text-xl font-semibold'>Apps Review Chart</h3>
-            <div className='bg-base-100 border rounded-xl p-4 h-80' >
-                <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                        width={500}
-                        height={300}
-                        data={singleAppData.ratings}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey='name' />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="count" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-                        <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-                    </BarChart>
-                </ResponsiveContainer>
+            <div className='space-y-3' >
+                <h3 className='text-xl font-semibold'>Apps Review Chart</h3>
+                <div className='bg-base-100 border rounded-xl p-4 h-80' >
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={singleAppData.ratings}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey='name' />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="count" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                            <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                        </BarChart>
+                    </ResponsiveContainer>
 
+                </div>
             </div>
-        </div>
             <div>
                 <h2 className='text-3xl font-semibold text-purple-800' >Description</h2>
                 <p>{description}</p>
             </div>
-
+            <ToastContainer />
         </div>
 
     );
